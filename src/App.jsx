2e009755e8057efe8242aa2c59eacb690b1201ad
justice jennings <justice.l.jennings@gmail.com>
2023-11-13 16:44:34 -0500

    fixed errors in coding, now displaying puppy array in console.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import {puppyList} from './data.js'
+import {puppyList} from './data.js';
+import { useState } from 'react';
+import "./App.css";
 
 function App() {
+  console.log(puppyList);
 
   return (
     <>
       <div>
-        
+        <p>hello</p>
       </div>
     </>
   )
 }
 
 
+export default App;
